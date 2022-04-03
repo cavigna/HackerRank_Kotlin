b704b1bb62a06be12beacc0ece6d1136e13e80fd
cavigna <40524472+cavigna@users.noreply.github.com>
2022-04-03 14:40:32 -0400

    Added caesarCipher

--- src/main/kotlin/OneWeek.kt
+++ src/main/kotlin/OneWeek.kt
@@ -94,70 +94,134 @@ fun lonelyinteger(a: Array<Int> = arrayOf(1, 2, 3, 4, 3, 2, 1)): Int {
 }
 
 fun diagonalDifference(arr: Array<Array<Int>>): Int {
-   // println(arr.contentDeepToString())
+    // println(arr.contentDeepToString())
 
     //import kotlin.math.abs
     var m = 0
     var n = 0
     var k = 0
-    var l = arr.size -1
+    var l = arr.size - 1
     var suma1 = 0
     var suma2 = 0
 
-    for (i in 0 until arr.size){
+    for (i in 0 until arr.size) {
         suma1 += arr[m][n]
-        m +=1
-        n +=1
+        m += 1
+        n += 1
 
         suma2 += arr[k][l]
-        k +=1
-        l -=1
+        k += 1
+        l -= 1
     }
     //println(abs(suma1-suma2))
 
-    return abs(suma1-suma2)
+    return abs(suma1 - suma2)
 
 }
 
-fun main() {
 
+fun countingSortsss(arr: Array<Int> = arrayOf(1, 1, 3, 2, 1)): Array<Int> {
 
-    diagonalDifference(
-        arrayOf(
-            arrayOf(1, 2, 3),
-            arrayOf(4, 5, 6),
-            arrayOf(9, 8, 9)
-        )
-    )
-}
+    val mapa = arr.groupingBy { it }.eachCount()
+    println(mapa)
 
+    val listado = mutableListOf<Int>()
+    val array = Array(100) { 0 }
+    mapa.forEach { (k, v) ->
+        array[k] = v
 
-fun diagonalDifferencessss(arr: Array<Array<Int>>): Int {
-    println(arr.contentDeepToString())
-
-    //import kotlin.math.abs
-    var m = 0
-    var n = 0
-    var k = 0
-    var l = arr.size -1
-    var suma1 = 0
-    var suma2 = 0
-
-    for (i in 0 until 3){
-        suma1 += arr[m][n]
-        m +=1
-        n +=1
-
-        suma2 += arr[k][l]
-        k +=1
-        l -=1
     }
 
 
-    println(suma1)
-    println(suma2)
-    println(abs(suma1-suma2))
+    mapa.forEach { k, v ->
+        (0 until v).forEach {
+            listado.add(k)
+        }
+    }
 
-    return abs(suma1-suma2)
+
+
+    println(array.toList())
+    println(listado)
+
+
+
+    return emptyArray()
 
 }
+
+fun countingSort(arr: Array<Int> = arrayOf(1, 1, 3, 2, 1)): Array<Int> {
+
+    val array = Array(100) { 0 }
+
+    arr.groupingBy { it }.eachCount().forEach { (k, v) ->
+        array[k] = v
+
+    }
+
+    return array
+
+}
+
+/**
+ * Original alphabet:      abcdefghijklmnopqrstuvwxyz
+Alphabet rotated +3:    defghijklmnopqrstuvwxyzabc
+
+Original alphabet:      abcdefghijklmnopqrstuvwxyz
+Alphabet rotated +2:    cdefghijklmnopqrstuvwxyzab
+
+m -> o
+i -> k
+d -> f
+d -> f
+l -> n
+e -> g
+-    -
+O -> Q
+u -> w
+t -> v
+z -> b
+ */
+
+
+fun caesarCipher(s: String = "middle-Outz", k: Int = 2): String {
+
+
+    val listaMutable = mutableListOf<String>()
+
+    s.forEach {
+        when (it.isLetter()) {
+            true -> {
+
+
+                var n = if (it.code +k > 122) (96 +(it.code +k - 122)) else it.code+k
+                //while (n<)
+
+                listaMutable.add(n.toChar().toString())
+
+            }
+            false -> {
+                listaMutable.add(it.toString())
+            }
+        }
+    }
+
+    println(listaMutable.joinToString(""))
+
+    return s
+
+}
+
+
+fun main() {
+    caesarCipher()
+    caesarCipher("There's-a-starman-waiting-in-the-sky", 8)
+
+
+
+}
+
+
+
+
+
